5dd10874ff14116012cb5d8ecbe92375a1139d7b
asa-o <asao.develop@gmail.com>
2024-02-04 07:22:27 +0900

    画面更新追加

--- python/rch-keepalive/keep.py
+++ python/rch-keepalive/keep.py
@@ -4,13 +4,20 @@ import random
 
 # 実行時は Weatherである前提
 
+def refresh():
+    time.sleep(0.3)
+    pyautogui.press('f5')
+
 # left news change
 pyautogui.click(75, 715)
 
-time.sleep(1)
+refresh()
+time.sleep(0.2)
 
 # right news change
 pyautogui.click(715, 750)
+
+refresh()
 
 # いくらかの待機
 wait_time = random.randint(60, 70)
@@ -19,7 +26,10 @@ time.sleep(wait_time)
 # left weather change
 pyautogui.click(70, 870)
 
-time.sleep(1)
+refresh()
+time.sleep(0.2)
 
 # right weather change
 pyautogui.click(700, 905)
+
+refresh()
