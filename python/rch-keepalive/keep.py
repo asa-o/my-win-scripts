0928107252ee7c5200d542cbd29eb14d8a6a704b
asa-o <asao.develop@gmail.com>
2024-03-17 13:29:36 +0900

    更新後はpageupを入力しておく

--- python/rch-keepalive/keep.py
+++ python/rch-keepalive/keep.py
@@ -19,6 +19,8 @@ RIGHT_WEATHER = (700, 840)
 def refresh():
     time.sleep(0.5)
     pyautogui.press('f5')
+    time.sleep(0.5)
+    pyautogui.press('pageup')
 
 def randDuration():
     return random.randint(10, 100) / 10
